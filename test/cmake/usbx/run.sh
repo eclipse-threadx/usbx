bd99a4f886f61815052ab061dea71578fab498e8
CQ Xiao <chxiao@microsoft.com>
2023-11-14 13:24:42 +0800

    Modify script.

--- test/cmake/usbx/run.sh
+++ test/cmake/usbx/run.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 cd $(dirname $0)
-[ -f .run.sh ] || ln -sf ../../externals/threadx/scripts/cmake_bootstrap.sh .run.sh
-if ! grep -q "\-\-output\-junit \$1.xml" .run.sh; then
-  sed -i 's/ctest $parallel --timeout 1000 -O $1.txt/& --output-junit $1.xml/g' .run.sh
+if ! grep -q "\-\-output\-junit \$1.xml" ../../externals/threadx/scripts/cmake_bootstrap.sh; then
+  sed -i 's/ctest $parallel --timeout 1000 -O $1.txt/& --output-junit $1.xml/g' ../../externals/threadx/scripts/cmake_bootstrap.sh
 fi
+[ -f .run.sh ] || ln -sf ../../externals/threadx/scripts/cmake_bootstrap.sh .run.sh
 ./.run.sh $*
