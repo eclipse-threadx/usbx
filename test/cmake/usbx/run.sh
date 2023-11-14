63555c2858d5bacde9d0fcd9f1907c9ebbd59c5f
CQ Xiao <chxiao@microsoft.com>
2023-11-14 13:08:08 +0800

    Modify ctest command to generate junit file.

--- test/cmake/usbx/run.sh
+++ test/cmake/usbx/run.sh
@@ -2,4 +2,7 @@
 
 cd $(dirname $0)
 [ -f .run.sh ] || ln -sf ../../externals/threadx/scripts/cmake_bootstrap.sh .run.sh
+if ! grep -q "\-\-output\-junit \$1.xml" .run.sh; then
+  sed -i 's/ctest $parallel --timeout 1000 -O $1.txt/& --output-junit $1.xml/g' .run.sh
+fi
 ./.run.sh $*
