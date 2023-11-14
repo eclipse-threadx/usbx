009c3fd0a1222fca914d1a84ac8354d0b7bba73f
CQ Xiao <53201544+xiaocq2001@users.noreply.github.com>
2023-11-14 23:59:55 +0000

    Fix testing error.

--- common/usbx_device_classes/src/ux_device_class_hid_receiver_tasks_run.c
+++ common/usbx_device_classes/src/ux_device_class_hid_receiver_tasks_run.c
@@ -165,10 +165,12 @@ ULONG                               temp;
                 return(UX_STATE_NEXT);
             }
 
+            /* Get current position.  */
+            pos = receiver -> ux_device_class_hid_receiver_event_save_pos;
+
 #if (UX_DEVICE_ENDPOINT_BUFFER_OWNER == 1) && defined(UX_DEVICE_CLASS_HID_ZERO_COPY)
 
             /* Save received event length.  */
-            pos = receiver -> ux_device_class_hid_receiver_event_save_pos;
             temp = transfer -> ux_slave_transfer_request_actual_length;
 #else
 
